e54ace032d9270c4e2378d01f34ce1309d2da877
leemeem14 <leemeem19@naver.com>
2025-06-04 12:06:37 +0900

    Initial commit

---
+++ src/main/java/kr/ac/kopo/lyh/personalcolor/PersonalColorApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.lyh.personalcolor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalColorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalColorApplication.class, args);
+    }
+
+}
---
+++ src/main/java/kr/ac/kopo/lyh/personalcolor/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package kr.ac.kopo.lyh.personalcolor.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+
+public class SecurityConfig {
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/", "/shop", "/login", "/signup", "/css/**", "/js/**", "/images/**").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin()
+//                .loginPage("/login")
+//                .defaultSuccessUrl("/shop", true)
+//                .and()
+//                .logout()
+//                .logoutSuccessUrl("/");
+//    }
+}
+
